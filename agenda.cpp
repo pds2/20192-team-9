a61ec675398c6a03c1b12088e6bb783a201d6d03
CancadoCyrino <55508536+CancadoCyrino@users.noreply.github.com>
2019-10-21 16:13:45 -0300

    Update agenda.cpp

--- agenda.cpp
+++ agenda.cpp
@@ -1,17 +1,16 @@
 #include "agenda.h"
 #include <iostream>
 #include <stdbool.h>
-
 Agenda::Agenda() 
 {
-   inicio = nullptr;
-   fim = nullptr;
+   this->inicio = nullptr;
+   this->fim = nullptr;
 }
 
 Agenda::~Agenda() 
 {
-   node *anterior = nullptr;
-   node *proximo = inicio;
+   Node *anterior = nullptr;
+   Node *proximo = this->inicio;
    while (proximo != nullptr) {
      anterior = proximo;
      proximo = proximo->proximo;
@@ -25,15 +24,15 @@ void Agenda::Marcar_consulta()
    novo->proximo=nullptr;
    novo->anterior=nullptr;
    int min, hora, dia, mes, ano;
-   std::cout<<"Digite o horario da consulta(hora cheia em seguida os minutos):"<<endl;
+   std::cout<<"Digite o horario da consulta(hora cheia em seguida os minutos):"<<"\n";
    std::cin>>hora>>min;
-   std::cout<<"Digite a data da consulta( DD/MM/AAAA ):"<<endl;
+   std::cout<<"Digite a data da consulta( DD/MM/AAAA ):"<<"\n";
    std::cin>>dia>>mes>>ano;
-   novo->Data_consulta->min=min;
-   novo->Data_consulta->hora=hora;
-   novo->Data_consulta->mes=mes;
-   novo->Data_consulta->dia=dia;
-   novo->Data_consulta->ano=ano;
+   novo->data.min=min;
+   novo->data.hora=hora;
+   novo->data.mes=mes;
+   novo->data.dia=dia;
+   novo->data.ano=ano;
    if(this->inicio==nullptr)
    {
    		this->inicio=novo;
@@ -64,9 +63,10 @@ void Agenda::Desmarcar_consulta(std::string paciente)
 
 void Agenda::get_consulta (std::string paciente)
 {
-	Node *aux=inicio;
+	Node *aux= this->inicio;
 	while(aux->nome != paciente)
 		aux=aux->proximo;
-	std::cout<<"o paciente "<< aux->nome << " tem uma consulta marcada para o dia "<< aux->Data_consulta->dia <<"/"<<aux->Data_consulta->mes<<"/"<<aux->Data_consulta->ano;
-	std::cout<<endl<< "às "<<aux->Data_consulta->hora<<":"<<aux->Data_consulta->min;
+	std::cout<<"o paciente "<< aux->nome << " tem uma consulta marcada para o dia "<< aux->data.dia <<"/"<<aux->data.mes<<"/"<<aux->data.ano;
+	std::cout<<"\n"<< "às "<<aux->data.hora<<":"<<aux->data.min;
 }
+
