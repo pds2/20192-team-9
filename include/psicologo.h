35a94f9b12d0e7c40741b658fecd494c1736f11d
ccancado <cybele.cancado@gmail.com>
2019-11-01 19:24:06 -0300

    psicologo implementação

--- include/psicologo.h
+++ include/psicologo.h
@@ -1,18 +1,31 @@
 #include <string>
 #include "pessoa.h"
+#include "funcionario.h"
+#include "agenda.h"
+#include "paciente.h"
 
-class Psicologo : Pessoa {
+
+class Psicologo : Pessoa, Funcionario {
 	private:
-		std::string numeroCRP;
+		std::string CRP;
+		Agenda * agenda;
 	public:
-		Psicologo(std::string numeroCRP, std::string id, std::string nome, std::string endereco, std::string telefone, std::string dataInicio);
+		Psicologo(std::string CRP, std::string CPF, std::string nome, std::string endereco, std::string telefone, std::string dataInicio);
 		~Psicologo();
-		void imprimirDados();
-		void setProntuarioPaciente();
+
+		std::string consultarAgendaDia(Psicologo * p) override;
+		std::string proximaConsulta(Psicologo * p) override;
+		std::string adicionarConsulta(Psicologo * p, std::string dia, std::string mes, std::string ano, std::string paciente, std::string hora) override;
+
+		void desmarcarConsulta(Psicologo * p, std::string dia, std::string mes, std::string ano, std::string paciente, std::string hora) override;
+		void editarProntuario(Paciente * p, std::string entrada);
+
 
 		// SETTERS
-		void setNumeroCRP(std::string numeroCRP);
+		void setCRP(std::string CRP);
+		void setAgenda(Agenda agenda);
 
 		// GETTERS
-		std::string getNumeroCRP();
+		std::string getCRP();
+		Agenda getAgenda();
 };
--- src/psicologo.cpp
+++ src/psicologo.cpp
@@ -1,27 +1,49 @@
 #include "psicologo.h"
 
 Psicologo::Psicologo(
-			std::string numeroCRP,
-			std::string id,
+			std::string CRP,
 			std::string nome,
 			std::string endereco,
 			std::string telefone,
-			std::string dataInicio) : Pessoa(id, nome, endereco, telefone, dataInicio), numeroCRP(numeroCRP) { }
-
-Psicologo::~Psicologo(){}
-
-void Psicologo::imprimirDados(){
-
+			std::string dataInicio) : Pessoa(CPF, nome, endereco, telefone, dataInicio), CRP(CRP) { 
+				this->agenda = new Agenda();
 }
 
-void Psicologo::setProntuarioPaciente(){
-
+Psicologo::~Psicologo(){
+	delete this->agenda;
 }
 
-void Psicologo::setNumeroCRP(std::string numeroCRP){
-	this->numeroCRP=numeroCRP;
+std::string Psicologo::consultarAgendaDia(Psicologo * p){
+	return p->agenda->consultarAgendaDia();
 }
 
-std::string Psicologo::getNumeroCRP(){
-	return this->numeroCRP;
+std::string Psicologo::proximaConsulta(Psicologo * p){
+	return p->agenda->proximaConsulta();
+}
+
+std::string Psicologo::adicionarConsulta(Psicologo * p, std::string dia, std::string mes, std::string ano, std::string paciente, std::string hora){
+	return p->agenda->adicionarConsulta(dia, mes, ano, paciente, hora);
+}
+
+std::string Psicologo::desmarcarConsulta(Psicologo * p, std::string dia, std::string mes, std::string ano, std::string paciente, std::string hora){
+	return p->agenda->desmarcarConsulta(dia, mes, ano, paciente, hora);
+}
+
+void Psicologo::editarProntuario(Paciente * p, std::string entrada){
+	p->addEntrada(this->CRP, entrada);
+}
+
+void Psicologo::setCRP(std::string CRP){
+	this->CRP=CRP;
+}
+void Psicologo::setAgenda(Agenda * agenda){
+	this->agenda = agenda;
+}
+
+std::string Psicologo::getCRP(){
+	return this->CRP;
+}
+
+Agenda Psicologo::getAgenda(){
+	return this->agenda;
 }
