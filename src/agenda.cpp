64ec9474e6698608948b4b3f03d533807162ab16
CancadoCyrino <55508536+CancadoCyrino@users.noreply.github.com>
2019-11-10 20:33:18 -0300

    atualizacao agenda.cpp

--- src/agenda.cpp
+++ src/agenda.cpp
@@ -1,10 +1,63 @@
 #include "agenda.h"
 
-Agenda::Agenda() 
+int CalcularDiaDaSemana(int d, int m, int a)
 {
-   this-> inicio = nullptr;
-   this-> fim = nullptr;
-   this-> proxConsulta = nullptr;
+	int k;
+	if(m==1)
+	{
+		m=13;
+		a=a-1;
+	}
+	else if(m==2)
+	{
+		m=14;
+		a=a-1;
+	}
+
+	k=(d+(2*m)+(3*(m+1)/5)+a+(a/4)-(a/100)+(a/400)+2);
+
+	k=(k/7);
+
+	if (k==0 || k==1)
+		return 0;
+	else
+		return 1;
+}
+
+int VerificarHoraConsulta(int h)
+{
+	if (h<7 || h>17)
+	{
+		return 0;
+	}
+	else
+		return 1;
+}
+
+std::string gerarId(int dia, int mes, int ano)
+{
+	std::string id="";
+	id += std::to_string(ano);
+	if (mes<10)
+		id += "0";
+	id += std::to_string(mes);
+	if(dia<10)
+		id += "0";
+	id += std::to_string(dia);
+
+	return id;
+
+}
+
+
+Agenda::Agenda()
+{
+	std::cout<<"Quais sao os horarios deste psicologo? (ex.: 17h = 17 , 09h = 9)";
+	for (int i = 0; i < 5; i++)
+	{
+		std::cin>>this -> hora[i];
+		std::cout<<std::endl;
+	}
 }
 
 Agenda::~Agenda() 
@@ -19,194 +72,251 @@ Agenda::~Agenda()
    }
 }
 
-void Agenda::adicionarConsulta(std::string paciente, int dia, int mes, int ano, int hora, int min)
+void Agenda::adicionarConsulta(std::string paciente, int dia, int mes, int ano, int hora)
 {
-	//cria uma nova consulta
-	Consulta *novo = new Consulta;
-	
-	//preenche as variaveis de consulta com os parametros passados
-	novo-> paciente = paciente;
-	novo-> data.dia = dia;
-	novo-> data.mes = mes;
-	novo-> data.ano = ano;
-	novo-> hora.hora = hora;
-	novo-> hora.min = min;
-	//cria o id da consulta
-	novo -> id += std::to_string(ano);
-	if(mes < 10)
-		novo -> id += "0";
-	novo -> id += std::to_string(mes);
-	if(dia < 10)
-		novo -> id += "0";
-	novo -> id += std::to_string(dia);
-	if(hora < 10)
-		novo -> id += "0";
-	novo -> id += std::to_string(hora);
-	if(min < 10)
-		novo -> id += "0";
-	novo -> id += std::to_string(min);
+	if (CalcularDiaDaSemana(dia, mes, ano)==0)
+	{
+		std::cout<<"A clinica so funciona de segunda a sexta";
+	}
+	else if(VerificarHoraConsulta(hora)==0)
+	{
+		std::cout<<"O horario de funcionamento da clinica e de 7h as 17h";
+	}
 
-	//caso nao exista nenhuma consulta na Agenda
-	if(this->inicio==nullptr)
-	{
-		this-> inicio = novo;
-		this-> proxConsulta = novo;
-		this-> fim = novo;
-	}
-	//caso seja a consulta com a data mais distante
-	else if(this -> fim -> id < novo -> id)
-	{
-		this -> fim -> proximo = novo;
-		novo -> anterior = this -> fim;
-		this -> fim = novo;
-	}
 
 	else
 	{
-		//cria um ponteiro que aponta para consulta mais recente e que percorre a agenda
-		Consulta *percorrer = this -> proxConsulta;
-		//cria um ponteiro auxiliar 
-		Consulta *aux = nullptr;
-		//percorre a lista ate encontrar a posicao da consulta em ordem cronologica
-		while(percorrer -> id < novo -> id)
-			percorrer = percorrer -> proximo;
+		//cria um ponteiro do tipo consulta para percorrer a agenda e verificar se ja existe umca consulta com esta data 
+		Consulta *percorre = this -> proxConsulta;
 
-		aux = percorrer -> anterior;
+		bool existeConsulta = false;
+		//variavel com o id da consulta
+		std::string id = gerarId(dia, mes, ano);
 
-		aux -> proximo = novo;
-		novo -> anterior = aux;
-		novo -> proximo = percorrer;
-		percorrer -> anterior = novo;
+		//percorre a agenda da proxima consulta ate o fim
+		while(percorre -> proximo != nullptr)
+		{
+			//verifica se ja existe uma consulta com o mesmo id
+			if(percorre -> id == id)
+			{
+				//se existir, variavel do tipo bool recebe true e sai do escopo da funcao while
+				existeConsulta = true;
+				break;
+			}
+			percorre = percorre -> proximo;
+		}
+
+		//caso nao exista consulta com o mesmo id
+		if (existeConsulta == false)
+		{
+			//cria uma nova consulta
+			Consulta *novo = new Consulta;
+		
+			//preenche as variaveis de consulta com os parametros passados
+			//novo-> paciente = paciente;
+			novo-> data.dia = dia;
+			novo-> data.mes = mes;
+			novo-> data.ano = ano;
+			novo-> id = id;
+			for (int i = 0; i < 5; i++)
+			{
+				novo ->  horario[i].hora = this -> hora[i];
+				if(novo->horario[i].hora==hora)
+					novo->horario[i].paciente = paciente;
+
+			}
+			//caso nao exista nenhuma consulta na Agenda
+			if(this->inicio==nullptr)
+			{
+				this-> inicio = novo;
+				this-> proxConsulta = novo;
+				this-> fim = novo;
+			}
+			//caso seja a consulta com a data mais distante
+			else if(this -> fim -> id < novo -> id)
+			{
+				this -> fim -> proximo = novo;
+				novo -> anterior = this -> fim;
+				this -> fim = novo;
+			}
+
+			else
+			{
+				//cria um ponteiro que aponta para consulta mais recente
+				Consulta *percorrer = this -> proxConsulta;
+				//cria um ponteiro auxiliar 
+				Consulta *aux = nullptr;
+				//percorre a lista ate encontrar a posicao da lista em ordem cronologica
+				while(percorrer -> id < novo -> id)
+					percorrer = percorrer -> proximo;
+
+				aux = percorrer -> anterior;
+
+				aux -> proximo = novo;
+				novo -> anterior = aux;
+				novo -> proximo = percorrer;
+				percorrer -> anterior = novo;
+			}
+		}
+
+		//caso ja exista consulta com mesmo id
+		else
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				if(percorre->horario[i].hora==hora)
+					percorre->horario[i].paciente = paciente;
+			}
+		}
 	}
+
 }
 
-void Agenda::desmarcarConsulta(int dia, int mes, int ano, int hora, int min)
+void Agenda::desmarcarConsulta(std::string paciente, int dia, int mes, int ano)
 {
-	//cria uma string para receber o id da consulta a ser desmarcada
-	std::string id = "";
-	//cria ponteiro para percorrer a Agenda e ponteiros auxiliares necessarios para deletar a consulta
-	Consulta *percorrer = this -> proxConsulta;
-	Consulta *anterior = nullptr , *proximo = nullptr;
+	//gera o id do dia passado
+	std::string id = gerarId(dia, mes, ano);
+	//variavel para verificar se existe qualquer consulta marcada no dia
+	bool existeConsulta = false;
+	//ponteiro para percorrer a agenda
+	Consulta *percorrer = proxConsulta;
+	//percorre a agenda
+	while(percorrer -> proximo != nullptr)
+	{
+		//caso exista qualquer consulta marcada no dia passado
+		if(percorrer -> id == id)
+			{
+				//se existir, variavel do tipo bool recebe true e sai do escopo da funcao while
+				existeConsulta = true;
+				break;
+			}
+			percorrer = percorrer -> proximo;
+	}
+	//caso nao exista nenhuma consulta marcada no dia passado
+	if(existeConsulta==false)
+		std::cout<<"Nao ha nenhuma consulta marcada neste dia";
 	
-	//preenche a string com a id da consulta a ser desmarcada
-	id += std::to_string(ano);
-	if(mes < 10)
-		id += "0";
-	id += std::to_string(mes);
-	if(dia < 10)
-		id += "0";
-	id += std::to_string(dia);
-	if(hora < 10)
-		id += "0";
-	id += std::to_string(hora);
-	if(min < 10)
-		id += "0";
-	id += std::to_string(min);
-
-	//percorre a Agenda ate encontrar a consulta a ser removida
-	while(id != percorrer -> id)
-		percorrer = percorrer -> proximo;
-
-	//remove a consulta
-	anterior = percorrer -> anterior;
-	proximo = percorrer -> proximo;
-
-	anterior -> proximo = proximo;
-	proximo -> anterior = anterior;
-
-	delete percorrer;
-}
-
-void Agenda::desmarcarConsulta(std::string id)
-{
-	//cria ponteiro para percorrer a Agenda e ponteiros auxiliares necessarios para deletar a consulta
-	Consulta *percorrer = this -> proxConsulta;
-	Consulta *anterior = nullptr , *proximo = nullptr;
-
-	//percorre a Agenda ate encontrar a consulta a ser removida
-	while(id != percorrer -> id)
-		percorrer = percorrer -> proximo;
-
-	//remove a consulta
-	anterior = percorrer -> anterior;
-	proximo = percorrer -> proximo;
-
-	anterior -> proximo = proximo;
-	proximo -> anterior = anterior;
-
-	delete percorrer;
-}
-	
-
-std::string Agenda::proximaConsulta()
-{
-	//cria uma string que sera o retorno do metodo
-	std::string retorna = "";
-	//preenche a string com as informacoes da proxima consulta
-	retorna += this -> proxConsulta -> paciente;
-	retorna += "	no dia ";
-	retorna += std::to_string(this -> proxConsulta ->data.dia);
-	retorna += "/";
-	retorna += std::to_string(this -> proxConsulta ->data.mes);
-	retorna += "/";
-	retorna += std::to_string(this -> proxConsulta ->data.ano);
-	retorna += "	as ";
-	retorna += std::to_string(this -> proxConsulta -> hora.hora);
-	retorna += "h";
-	retorna += std::to_string(this -> proxConsulta -> hora.min);
-	retorna += "min.";
-
-	return retorna;
+	//caso existam consultas marcadas no dia passado
+	else
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			if(percorrer -> horario[i].paciente == paciente)
+			{
+				//torna o horario disponivel neste dia
+				percorrer ->horario[i].paciente = "disponivel";
+				break;
+			}
+			//caso nao tenha consultas marcadas para o paciente passado
+			std::cout<<paciente<<" nao tem consulta marcada neste dia";
+		}
+	}
 }
 
 std::string Agenda::consultarAgendaDia(int dia, int mes, int ano)
 {
-	//cria duas strings auxiliares que contem o menor id possivel do dia passado e o maior id possivel do dia passado. 
-	std::string menorDia = "";
-	std::string maiorDia = "";
-	//cria a string que sera retornada.
-	std::string retorna = "";
-	//cria ponteiro auxiliar para percorrer a Agenda
-	Consulta *percorrer = this -> proxConsulta;
-
-	//preenche as strings menorDia e maiorDia
-	menorDia += std::to_string(ano);
-	maiorDia += std::to_string(ano);
-	if(mes < 10)
+	//cria a string que sera retornada
+	std::string retorna="";
+	//gera o id do dia passado
+	std::string id = gerarId(dia, mes, ano);
+	//variavel para verificar se existe qualquer consulta marcada no dia
+	bool existeConsulta = false;
+	//ponteiro para percorrer a agenda
+	Consulta *percorrer = proxConsulta;
+	//percorre a agenda
+	while(percorrer -> proximo != nullptr)
 	{
-		menorDia += "0";
-		maiorDia += "0";
+		//caso exista qualquer consulta marcada no dia passado
+		if(percorrer -> id == id)
+			{
+				//se existir, variavel do tipo bool recebe true e sai do escopo da funcao while
+				existeConsulta = true;
+				break;
+			}
+			percorrer = percorrer -> proximo;
 	}
-	menorDia += std::to_string(mes);
-	maiorDia += std::to_string(mes);
-	if(dia < 10)
+	//caso nao exista nenhuma consulta marcada no dia passado
+	if(existeConsulta==false)
+		retorna="Nao ha nenhuma consulta marcada neste dia";
+	
+	//caso existam consultas marcadas no dia passado
+	else
 	{
-		menorDia += "0";
-		maiorDia += "0";
+		for (int i = 0; i < 5; i++)
+		{
+			if (percorrer->horario[i].paciente=="disponivel")
+			{
+				retorna +="horario das ";
+				retorna += std::to_string(percorrer->horario[i].hora);
+				retorna +="h esta disponivel		";
+			}
+			else
+			{
+				retorna += percorrer -> horario[i].paciente;
+				retorna += "	as ";
+				retorna += std::to_string(percorrer -> horario[i].hora);
+				retorna += "h 		";
+			}
+		}
 	}
-	menorDia += std::to_string(dia);
-	maiorDia += std::to_string(dia);
-	maiorDia += "24";
-	for(int i = 0; i <2; i++)
-		maiorDia += "0";
-	for(int i = 0; i < 4; i++)
-		menorDia += "0";
+	return retorna;
+}
 
-
-	//percorre a agenda ate chegar no dia desejado
-	while(percorrer -> id < menorDia)
-		percorrer = percorrer -> proximo;
-
-
-	//adiciona as informaçoes necessarias a string que sera retornada
-	while(percorrer -> id <= maiorDia)
+std::string Agenda::horarioDisponivel(int dia, int mes, int ano)
+{
+	//gera o id do dia passado
+	std::string id = gerarId(dia, mes, ano);
+	//cria a string que sera retornada
+	std::string retorna="";
+	//variavel para verificar se existe qualquer consulta marcada no dia
+	bool existeConsulta = false;
+	//ponteiro para percorrer a agenda
+	Consulta *percorrer = proxConsulta;
+	//percorre a agenda
+	while(percorrer -> proximo != nullptr)
 	{
-		retorna += percorrer -> paciente;
-		retorna += "	as ";
-		retorna += std::to_string(percorrer -> hora.hora);
-		retorna += "h";
-		retorna += std::to_string(percorrer -> hora.min);
-		retorna += "min.		";
+		//caso exista qualquer consulta marcada no dia passado
+		if(percorrer -> id == id)
+			{
+				//se existir, variavel do tipo bool recebe true e sai do escopo da funcao while
+				existeConsulta = true;
+				break;
+			}
+			percorrer = percorrer -> proximo;
+	}
+	//caso nao exista nenhuma consulta marcada com esta data
+	if (existeConsulta==false)
+	{
+		int j = CalcularDiaDaSemana(dia, mes, ano);
+		//caso seja um final de semana
+		if (j==0)
+		{
+			retorna = "A data passada e um final de semana. A clinica so funciona de segunda a sexta.";
+		}
+		//caso seja durante a semana
+		else
+		{
+			retorna="Todos os horarios estao disponiveis nesta data";
+		}
+	}
+	//caso exista consulta marcada com a data passada
+	else
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			if (percorrer->horario[i].paciente=="disponivel")
+			{
+				retorna += "O horario de ";
+				retorna += std::to_string(percorrer->horario[i].hora);
+				retorna += "h esta disponivel";
+			}
+			else
+			{
+				retorna += "O horario de ";
+				retorna += std::to_string(percorrer->horario[i].hora);
+				retorna += "h nao esta disponivel";
+			}
+		}
 	}
 
 	return retorna;
@@ -214,7 +324,5 @@ std::string Agenda::consultarAgendaDia(int dia, int mes, int ano)
 
 void Agenda::fimConsulta()
 {
-	this -> proxConsulta = this -> proxConsulta -> proximo;	
+	proxConsulta = proxConsulta -> proximo;
 }
-
-
